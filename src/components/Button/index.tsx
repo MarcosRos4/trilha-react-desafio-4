19f8c36696ab756b99a9313b0bcc734cfcce897b
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2024-09-30 02:02:02 +0000

    a

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,19 @@
-import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, disabled, onClick }: IButtonProps) => {
+  const estiloBotao = {
+    width: '100%',
+    height: '42px',
+    backgroundColor: disabled ? '#A9A9A9' : '#81259D', // Muda a cor para cinza se desabilitado
+    color: '#FFF',
+    border: `1px solid ${disabled ? '#A9A9A9' : '#81259D'}`, // Muda a cor da borda para cinza se desabilitado
+    borderRadius: '21px',
+    cursor: disabled ? 'not-allowed' : 'pointer', // Muda o cursor se desabilitado
+    transition: 'opacity 0.3s', // Adiciona uma transição suave para o hover
+    opacity: disabled ? 0.6 : 1, // Ajusta a opacidade se desabilitado
+  };
+
+  return <button disabled={disabled} style={estiloBotao} onClick={onClick}>{title}</button>;
 };
 
 export default Button;
--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import backgroundImage from "./image.png"
 
 export default createGlobalStyle`
     * {
@@ -13,6 +14,9 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         background-color: #81259D;
+        background-image: url(${backgroundImage});
+        background-size: cover;
+        background-position: center
     }
 
     input, button, textarea, h1, h2, h3, h4, h5, h6 {
